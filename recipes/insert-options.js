3399cc8bc9ec0e79d1cc6f9e191f6153302efdea
erhan <erhan.16k@gmail.com>
2024-02-24 11:18:15 +0100

    inderting recipe values

---
+++ recipes/insert-options.js
@@ -0,0 +1,315 @@
+const insert_options = {
+    recipes: [
+        "No-Bake Cheesecake",
+        "Roasted Brussels Sprouts",
+        "Mac & Cheese",
+        "Tamagoyaki Japanese Omelette"
+    ],
+    categories: [
+        "Cake",
+        "No-Bake",
+        "Vegetarian",
+        "Vegan",
+        "Gluten-Free",
+        "Japanese"
+    ],
+    ingredients: [
+        "Condensed milk",
+        "Cream Cheese",
+        "Lemon Juice",
+        "Pie Crust",
+        "Cherry Jam",
+        "Brussels Sprouts",
+        "Lemon juice",
+        "Sesame seeds",
+        "Pepper",
+        "Salt",
+        "Olive oil",
+        "Macaroni",
+        "Butter",
+        "Flour",
+        "Milk",
+        "Shredded Cheddar cheese",
+        "Eggs",
+        "Soy sauce",
+        "Sugar"
+    ],
+    steps: [
+        "Beat Cream Cheese",
+        "Add condensed Milk and blend",
+        "Add Lemon Juice and blend",
+        "Add the mix to the pie crust",
+        "Spread the Cherry Jam",
+        "Place in refrigerator for 3h.",
+        "Preheat the oven",
+        "Mix the ingredients in a bowl",
+        "Spread the mix on baking sheet",
+        "Bake for 30'",
+        "Cook Macaroni for 8'",
+        "Melt butter in a saucepan",
+        "Add flour, salt, pepper and mix",
+        "Add Milk and mix",
+        "Cook until mix is smooth",
+        "Add cheddar cheese",
+        "Add the macaroni",
+        "Beat the eggs",
+        "Add soya sauce, sugar and salt",
+        "Add oil to a sauce pan",
+        "Bring to medium heat",
+        "Add some mix to the sauce pan",
+        "Let is cook for 1'",
+        "Remove pan from fire"
+    ],
+    recipe_categories: [
+        {
+            recipe_id: 1,
+            category_id: 1
+        },
+        {
+            recipe_id: 1,
+            category_id: 2
+        },
+        {
+            recipe_id: 1,
+            category_id: 3
+        },
+        {
+            recipe_id: 2,
+            category_id: 4
+        },
+        {
+            recipe_id: 2,
+            category_id: 5
+        },
+        {
+            recipe_id: 3,
+            category_id: 3
+        },
+        {
+            recipe_id: 4,
+            category_id: 3
+        },
+        {
+            recipe_id: 3,
+            category_id: 6
+        }
+    ],
+    recipe_ingredients: [
+        {
+            recipe_id: 1,
+            ingredient_id: 1
+        },
+        {
+            recipe_id: 1,
+            ingredient_id: 2
+        },
+        {
+            recipe_id: 1,
+            ingredient_id: 3
+        },
+        {
+            recipe_id: 1,
+            ingredient_id: 4
+        },
+        {
+            recipe_id: 1,
+            ingredient_id: 5
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 6
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 7
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 8
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 9
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 10
+        },
+        {
+            recipe_id: 2,
+            ingredient_id: 11
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 12
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 13
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 14
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 10
+        },
+        {
+            recipe_id:3,
+            ingredient_id: 9
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 15
+        },
+        {
+            recipe_id: 3,
+            ingredient_id: 16
+        },
+        {
+            recipe_id: 4,
+            ingredient_id: 17
+        },
+        {
+            recipe_id: 4,
+            ingredient_id: 18
+        },
+        {
+            recipe_id: 4,
+            ingredient_id: 19
+        },
+        {
+            recipe_id: 4,
+            ingredient_id: 10
+        },
+        {
+            recipe_id: 4,
+            ingredient_id: 11
+        }
+    ],
+    recipe_steps: [
+        {
+            recipe_id: 1,
+            step_id: 1,
+            step_number: 1
+        },
+        {
+            recipe_id: 1,
+            step_id: 2,
+            step_number: 2
+        },
+        {
+            recipe_id: 1,
+            step_id: 3,
+            step_number: 3
+        },
+        {
+            recipe_id: 1,
+            step_id: 4,
+            step_number: 4
+        },
+        {
+            recipe_id: 1,
+            step_id: 5,
+            step_number: 5
+        },
+        {
+            recipe_id: 1,
+            step_id: 6,
+            step_number: 6
+        },
+        {
+            recipe_id: 2,
+            step_id: 7,
+            step_number: 1
+        },
+        {
+            recipe_id: 2,
+            step_id: 8,
+            step_number: 2
+        },
+        {
+            recipe_id: 2,
+            step_id: 9,
+            step_number: 3
+        },
+        {
+            recipe_id: 2,
+            step_id: 10,
+            step_number: 4
+        },
+        {
+            recipe_id: 3,
+            step_id: 11,
+            step_number: 1
+        },
+        {
+            recipe_id: 3,
+            step_id: 12,
+            step_number: 2
+        },
+        {
+            recipe_id: 3,
+            step_id: 13,
+            step_number: 3
+        },
+        {
+            recipe_id: 3,
+            step_id: 14,
+            step_number: 4
+        },
+        {
+            recipe_id: 3,
+            step_id: 15,
+            step_number: 5
+        },
+        {
+            recipe_id: 3,
+            step_id: 16,
+            step_number: 6
+        },
+        {
+            recipe_id: 3,
+            step_id: 17,
+            step_number: 7
+        },
+        {
+            recipe_id: 4,
+            step_id: 18,
+            step_number: 1
+        },
+        {
+            recipe_id: 4,
+            step_id: 19,
+            step_number: 2
+        },
+        {
+            recipe_id: 4,
+            step_id: 20,
+            step_number: 3
+        },
+        {
+            recipe_id: 4,
+            step_id: 21,
+            step_number: 4
+        },
+        {
+            recipe_id: 4,
+            step_id: 22,
+            step_number: 5
+        },
+        {
+            recipe_id: 4,
+            step_id: 23,
+            step_number: 6
+        },
+        {
+            recipe_id: 4,
+            step_id: 24,
+            step_number: 7
+        }
+    ]
+}
+
+module.exports = insert_options;
--- recipes/insert-values.js
+++ recipes/insert-values.js
@@ -1,5 +1,6 @@
 const util = require('util');
 const mysql = require('mysql');
+const insert_options = require('./insert-options');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -8,72 +9,6 @@ var connection = mysql.createConnection({
   database : 'recipes'
 });
 
-
-let insert_options = {
-    recipes: [
-        "No-Bake Cheesecake",
-        "Roasted Brussels Sprouts",
-        "Mac & Cheese",
-        "Tamagoyaki Japanese Omelette"
-    ],
-    categories: [
-        "Cake",
-        "No-Bake",
-        "Vegetarian",
-        "Vegan",
-        "Gluten-Free",
-        "Japanese"
-    ],
-    ingredients: [
-        "Condensed milk",
-        "Cream Cheese",
-        "Lemon Juice",
-        "Pie Crust",
-        "Cherry Jam",
-        "Brussels Sprouts",
-        "Lemon juice",
-        "Sesame seeds",
-        "Pepper",
-        "Salt",
-        "Olive oil",
-        "Macaroni",
-        "Butter",
-        "Flour",
-        "Pepper",
-        "Milk",
-        "Shredded Cheddar cheese",
-        "Eggs",
-        "Soy sauce",
-        "Sugar",
-        "Olive Oil"
-    ],
-    steps: [
-        "Beat Cream Cheese",
-        "Add condensed Milk and blend",
-        "Add Lemon Juice and blend",
-        "Add the mix to the pie crust",
-        "Spread the Cherry Jam",
-        "Place in refrigerator for 3h.",
-        "Preheat the oven",
-        "Mix the ingredients in a bowl",
-        "Spread the mix on baking sheet",
-        "Bake for 30'",
-        "Cook Macaroni for 8'",
-        "Melt butter in a saucepan",
-        "Add flour, salt, pepper and mix",
-        "Add Milk and mix",
-        "Cook until mix is smooth",
-        "Add cheddar cheese",
-        "Add the macaroni",
-        "Beat the eggs",
-        "Add soya sauce, sugar and salt",
-        "Add oil to a sauce pan",
-        "Bring to medium heat",
-        "Add some mix to the sauce pan",
-        "Let is cook for 1'",
-        "Remove pan from fire"
-    ]
-}
 connection.connect(err => {
     if (err) throw err;
     console.log('Connected to MySQL server.');
@@ -93,6 +28,15 @@ connection.connect(err => {
         }));
         await Promise.all(insert_options.steps.map(async instruction => {
             await execQuery(`INSERT INTO Steps (instruction) VALUES (?)`, [instruction]);
+        }));
+        await Promise.all(insert_options.recipe_categories.map(category =>{
+            execQuery('INSERT INTO RecipeCategories SET ?', category);
+        }));
+        await Promise.all(insert_options.recipe_ingredients.map(ingredient =>{
+            execQuery('INSERT INTO RecipeIngredients SET ?', ingredient);
+        }));
+        await Promise.all(insert_options.recipe_steps.map(step =>{
+            execQuery('INSERT INTO RecipeSteps SET ?', step);
         }));
     } catch (error) {
         console.error(error);
