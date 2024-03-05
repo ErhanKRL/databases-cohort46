607a3db3b9f10f2bd1ad67992827ba70d4101725
erhan <erhan.16k@gmail.com>
2024-03-05 09:41:15 +0100

    ex-4 mongoDb completed

--- Week3/homework/mongodb/index.js
+++ Week3/homework/mongodb/index.js
@@ -1,8 +1,8 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+require('dotenv').config();
 const { seedDatabase } = require("./seedDatabase.js");
 
-async function createEpisodeExercise(client) {
+async function createEpisodeExercise(episodesCollection) {
   /**
    * We forgot to add the last episode of season 9. It has this information:
    *
@@ -12,44 +12,65 @@ async function createEpisodeExercise(client) {
    */
 
   // Write code that will add this to the collection!
+    const episodeData = {
+      episode: "S09E13",
+      title: "MOUNTAIN HIDE-AWAY",
+      elements: ["CIRRUS", "CLOUDS", "CONIFER", "DECIDUOUS", "GRASS", "MOUNTAIN", "MOUNTAINS", "RIVER", "SNOWY_MOUNTAIN", "TREE", "TREES"]
+    };
 
-  console.log(
-    `Created season 9 episode 13 and the document got the id ${"TODO: fill in variable here"}`
-  );
+    const result = await episodesCollection.insertOne(episodeData);
+
+    console.log(
+      `Created season 9 episode 13 and the document got the id ${result.insertedId}`
+    );
 }
 
-async function findEpisodesExercises(client) {
+async function findEpisodesExercises(episodesCollection) {
   /**
    * Complete the following exercises.
    * The comments indicate what to do and what the result should be!
    */
 
   // Find the title of episode 2 in season 2 [Should be: WINTER SUN]
-
+  const episode2Season2 = await episodesCollection.findOne({ episode: "S02E02" });
+  if(!episode2Season2){
+    throw new Error(`Cannot find the episode ${episode2Season2}`);
+  }
   console.log(
-    `The title of episode 2 in season 2 is ${"TODO: fill in variable here"}`
+    `The title of episode 2 in season 2 is ${episode2Season2.title}`
   );
 
   // Find the season and episode number of the episode called "BLACK RIVER" [Should be: S02E06]
-
+  const blackRiverEpisode = await episodesCollection.findOne({ title: "BLACK RIVER" });
+  if(!blackRiverEpisode){
+    throw new Error(`Cannot find the episode ${blackRiverEpisode}`)
+  }
   console.log(
-    `The season and episode number of the "BLACK RIVER" episode is ${"TODO: fill in variable here"}`
+    `The season and episode number of the "BLACK RIVER" episode is ${blackRiverEpisode.episode}`
   );
 
   // Find all of the episode titles where Bob Ross painted a CLIFF [Should be: NIGHT LIGHT, EVENING SEASCAPE, SURF'S UP, CLIFFSIDE, BY THE SEA, DEEP WILDERNESS HOME, CRIMSON TIDE, GRACEFUL WATERFALL]
-
+  const cliffEpisodes = await episodesCollection.find({ elements: "CLIFF" }).toArray();
+  if(cliffEpisodes.length === 0){
+    throw new Error(`There is no "CLIFF" painting`);
+  }
+  const cliffEpisodesTitles = cliffEpisodes.map(episode => episode.title);
   console.log(
-    `The episodes that Bob Ross painted a CLIFF are ${"TODO: fill in variable here"}`
+    `The episodes that Bob Ross painted a CLIFF are ${cliffEpisodesTitles.join(', ')}`
   );
 
   // Find all of the episode titles where Bob Ross painted a CLIFF and a LIGHTHOUSE [Should be: NIGHT LIGHT]
-
+  const cliffAndLighthouseEpisodes = await episodesCollection.find({ elements: { $all: ["CLIFF", "LIGHTHOUSE"] } }).toArray();
+  if(cliffAndLighthouseEpisodes.length === 0){
+    throw new Error(`There is no "CLIFF and a LIGHTHOUSE" painting`);
+  }
+  const cliffAndLighthouseEpisodesTitles = cliffAndLighthouseEpisodes.map(episode => episode.title);
   console.log(
-    `The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE are ${"TODO: fill in variable here"}`
+    `The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE is ${cliffAndLighthouseEpisodesTitles.join(', ')}`
   );
 }
 
-async function updateEpisodeExercises(client) {
+async function updateEpisodeExercises(episodesCollection) {
   /**
    * There are some problems in the initial data that was filled in.
    * Let's use update functions to update this information.
@@ -58,28 +79,29 @@ async function updateEpisodeExercises(client) {
    */
 
   // Episode 13 in season 30 should be called BLUE RIDGE FALLS, yet it is called BLUE RIDGE FALLERS now. Fix that
-
+  const result = await episodesCollection.updateOne({ episode: "S30E13" }, { $set: { title: "BLUE RIDGE FALLS" } });
   console.log(
-    `Ran a command to update episode 13 in season 30 and it updated ${"TODO: fill in variable here"} episodes`
+    `Ran a command to update episode 13 in season 30 and it updated ${result.modifiedCount} episodes`
   );
 
   // Unfortunately we made a mistake in the arrays and the element type called 'BUSHES' should actually be 'BUSH' as sometimes only one bush was painted.
   // Update all of the documents in the collection that have `BUSHES` in the elements array to now have `BUSH`
   // It should update 120 episodes!
-
+  const updateResult = await episodesCollection.updateMany({ elements: "BUSHES" }, { $set: { "elements.$": "BUSH" } });
   console.log(
-    `Ran a command to update all the BUSHES to BUSH and it updated ${"TODO: fill in variable here"} episodes`
+    `Ran a command to update all the BUSHES to BUSH and it updated ${updateResult.modifiedCount} episodes`
   );
+  
 }
 
-async function deleteEpisodeExercise(client) {
+async function deleteEpisodeExercise(episodesCollection) {
   /**
    * It seems an errand episode has gotten into our data.
    * This is episode 14 in season 31. Please remove it and verify that it has been removed!
    */
-
+  const result = await episodesCollection.deleteOne({ episode: "S31E14" });
   console.log(
-    `Ran a command to delete episode and it deleted ${"TODO: fill in variable here"} episodes`
+    `Ran a command to delete episode and it deleted ${result.deletedCount} episodes`
   );
 }
 
@@ -97,21 +119,24 @@ async function main() {
 
   try {
     await client.connect();
+    const db = client.db();
 
     // Seed our database
     await seedDatabase(client);
 
+    const episodesCollection = db.collection("bob_ross_episodes");
+
     // CREATE
-    await createEpisodeExercise(client);
+    await createEpisodeExercise(episodesCollection);
 
     // READ
-    await findEpisodesExercises(client);
+    await findEpisodesExercises(episodesCollection);
 
     // UPDATE
-    await updateEpisodeExercises(client);
+    await updateEpisodeExercises(episodesCollection);
 
     // DELETE
-    await deleteEpisodeExercise(client);
+    await deleteEpisodeExercise(episodesCollection);
   } catch (err) {
     console.error(err);
   } finally {
